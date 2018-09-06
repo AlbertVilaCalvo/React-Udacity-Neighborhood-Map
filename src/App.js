9d59190386148d809163fc5001fd13fc0cf147b7
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2018-09-06 20:04:36 +0200

    Add left/right containers + hamburger menu icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import menu from './menu.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <div className='left-container'>
+          <h1 className='page-title'>Catalonia Locations</h1>
+          <ul>
+            <li>Some place</li>
+          </ul>
+        </div>
+        <div className='right-container'>
+          <img src={menu} alt='Menu'/>
+        </div>
       </div>
     );
   }
